9a6b7b6d7c91d063512c143aa5b730f18760120a
Phạm Trâm Anh <93437075+tramanhpham@users.noreply.github.com>
2023-12-20 12:43:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ class_list = {'0': 'Setosa', '1': 'Versicolor', '2': 'Virginica'}
 
 st.title('Iris classification based on sepal and petal size')
 
-image = Image.open('iris_flower.jpg')
-st.image(image)
-
 input = open('lrc_iris.pkl', 'rb')
 model = pkl.load(input)
 
